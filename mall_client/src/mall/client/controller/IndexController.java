132a8922c58a1803797e7fa0489fd92459a24059
GDJ29 <GDJ29@DESKTOP-SV454LT>
2021-04-12 18:17:25 +0900

    EbookDao.selectEbookListByPage() 메소드 생성

--- mall_client/src/mall/client/controller/IndexController.java
+++ mall_client/src/mall/client/controller/IndexController.java
@@ -1,6 +1,7 @@
 package mall.client.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,10 +10,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// C -> V (모두 컨트롤러를 지나간다) // Controller -> View | IndexController에서 요청하면 Index.jsp를 보여준다.
+import mall.client.model.EbookDao;
+import mall.client.vo.Ebook;
+
+// C -> M -> V (모두 컨트롤러를 지나간다) // Controller -> View | IndexController에서 요청하면 Index.jsp를 보여준다.
 @WebServlet("/IndexController")
 public class IndexController extends HttpServlet {
+	private EbookDao ebookDao;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// request 분석
+		int currentPage = 1;
+		if(request.getParameter("currentPage")!=null) {
+			currentPage = Integer.parseInt(request.getParameter("currentPage"));
+		}
+		int rowPerPage = 15;
+		int beginRow = (currentPage-1)*rowPerPage;
+		
+		// model 호출
+		this.ebookDao = new EbookDao();
+		List<Ebook> ebookList = this.ebookDao.selectEbookListByPage(beginRow, rowPerPage);
+		
+		// View forward
+		request.setAttribute("ebookList", ebookList);
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/index.jsp"); // WEB-INF안에 있어서 jsp로 바로 보여줄 수 없다
 		rd.forward(request, response);
 	}
---
+++ mall_client/src/mall/client/model/EbookDao.java
@@ -0,0 +1,38 @@
+package mall.client.model;
+
+import java.sql.*;
+import java.util.*;
+
+import mall.client.commons.DBUtil;
+import mall.client.vo.Ebook;
+
+public class EbookDao {
+	private DBUtil dbutil;
+	public List<Ebook> selectEbookListByPage(int beginRow, int rowPerPage) {// 부모타입으로 리턴
+		List<Ebook> list = new ArrayList<>();
+		this.dbutil = new DBUtil();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			conn = this.dbutil.getConnection();
+			String sql = "SELECT ebook_title ebookTitle, ebook_price ebookPrice FROM ebook ORDER BY ebook_date DESC LIMIT ?, ?";
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, beginRow);
+			stmt.setInt(2, rowPerPage);
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				Ebook ebook = new Ebook();
+				ebook.setEbookTitle(rs.getString("ebookTitle"));
+				ebook.setEbookPrice(rs.getInt("ebookPrice"));
+				list.add(ebook);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally { // try문을 실행하던 안하던 finally는 무조건 실행
+			this.dbutil.close(rs, stmt, conn);
+		}
+		return list;
+		
+	}
+}
